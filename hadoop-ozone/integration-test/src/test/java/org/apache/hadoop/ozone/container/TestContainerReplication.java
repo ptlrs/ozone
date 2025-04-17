844a48c3c6bc69fb400903731649f80a98d37e3c
Rishabh Patel <ptlrshb@gmail.com>
2025-04-16 22:11:45 -0700

    HDDS-12760. set timeout to 400

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/container/TestContainerReplication.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/ozone/container/TestContainerReplication.java
@@ -94,7 +94,7 @@ import org.slf4j.event.Level;
 /**
  * Tests ozone containers replication.
  */
-@Timeout(300)
+@Timeout(400)
 class TestContainerReplication {
 
   private static final String VOLUME = "vol1";
